05f63d06e67c39f457877ba349727f57943a57da
Brian Stolte <brian.stolte@gmail.com>
2015-08-19 18:19:37 +0000

    Connected user to places, added email to posts

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ActiveRecord::Base
+	
+	# Karminari setting for items to display per page... 
+	# https://github.com/amatsuda/kaminari#configuring-default-per_page-value-for-each-model
 	paginates_per 5
+
+	# Sets up an association with Users
+	belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Sets up an association with Places
+  has_many :places
 end
