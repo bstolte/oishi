83513774d5b2236fd5fdf6d03ae6a17fc067b8b5
Brian Stolte <brian.stolte@gmail.com>
2015-08-21 16:41:28 +0000

    Set up the places show page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,6 @@ class PlacesController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create]
 
 	def index
-		# @places = Place.all.page params[:page]
 		@places = Place.order(:name).page params[:page]
 	end
 
@@ -14,6 +13,12 @@ class PlacesController < ApplicationController
 		current_user.places.create(place_params)
 		redirect_to root_path
 	end
+
+	def show
+		@place = Place.find(params[:id])
+	end
+
+	private
 
 	def place_params
 		params.require(:place).permit(:name, :description, :address)
