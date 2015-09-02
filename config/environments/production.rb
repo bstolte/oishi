e23e129382c986a927fbc0b2feb8442a7ddea4ce
Brian Stolte <brian.stolte@gmail.com>
2015-09-02 14:58:45 +0000

    Added MailGun support for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,17 @@
 Oishi::Application.configure do
 
+  # Use Heroku's MailGun to send emails
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'oishi-yo.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
   # Set up for Devise auth
   config.action_mailer.default_url_options = { :host => 'oishi-yo.herokuapp.com' }
 
